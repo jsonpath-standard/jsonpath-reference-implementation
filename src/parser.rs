894df5028956f141248d3c77265487696c50d578
Glyn Normington <normingtong@vmware.com>
2020-09-01 12:27:26 +0100

    Move matcher pipeline out of parser
    Fixes https://github.com/glyn/jsonpath-ri/issues/5

--- src/parser.rs
+++ src/parser.rs
@@ -5,20 +5,15 @@
  */
 
 use crate::matchers;
-use crate::path::{FindError, Path};
+use crate::path;
 use crate::pest::Parser;
-use serde_json::Value;
 
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
 struct PathParser;
 
-struct SelectorPath<'a> {
-    matchers: Vec<&'a dyn matchers::Matcher>,
-}
-
 #[allow(clippy::single_match)]
-pub fn parse(selector: &str) -> Result<Box<dyn Path>, String> {
+pub fn parse(selector: &str) -> Result<Box<dyn path::Path>, String> {
     let selector_rule = PathParser::parse(Rule::selector, selector)
         .map_err(|e| format!("{}", e))?
         .next()
@@ -34,22 +29,5 @@ pub fn parse(selector: &str) -> Result<Box<dyn Path>, String> {
         }
     }
 
-    Ok(Box::new(SelectorPath { matchers: ms }))
-}
-
-impl Path for SelectorPath<'_> {
-    fn find<'a>(&self, document: &'a Value) -> Result<Vec<&'a Value>, FindError> {
-        // pass nodes, starting with document alone, through each matcher in turn
-        Ok((&self.matchers)
-            .iter()
-            .fold(doc_node(document), |nodes, matcher| {
-                nodes.iter().flat_map(|node| matcher.select(node)).collect()
-            }))
-    }
-}
-
-fn doc_node<'a>(document: &'a Value) -> Vec<&'a Value> {
-    let mut nodes = Vec::new();
-    nodes.push(document);
-    nodes
+    Ok(Box::new(path::new(ms)))
 }
